08eef77416a64fb9e90b1d9b15612f358682a178
Mikhail Yurasov <me@yurasov.me>
2016-02-12 21:45:22 -0800

    + Watchdog spec

---
+++ spec/Watchdog.spec.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const Watchdog = require('../src/lib/Commands/TestCommand/Watchdog');
+
+describe('Watchdog test suite', () => {
+
+  let w;
+
+  beforeEach(() => {
+    w = new Watchdog();
+  });
+
+  it('should emit timeout event in specified time', (done) => {
+
+    let tId;
+
+    w.timeout = 0.5;
+    w.start();
+
+    // this should be cancelled in 0.5s
+    tId = setTimeout(() => {
+      done.fail();
+    }, 555);
+
+    w.on('timeout', () => {
+      clearTimeout(tId);
+      done();
+    });
+
+  });
+
+  it('should stop watchdog timer', (done) => {
+
+    w.timeout = 0.5;
+    w.start();
+
+    // this should cancel watchdog
+    setTimeout(() => {
+      w.stop();
+    }, 444);
+
+    w.on('timeout', () => {
+      done.fail();
+    });
+
+    // done
+    setTimeout(done, 555);
+  });
+
+  it('should restart watchdog timer', (done) => {
+
+    const start = new Date();
+
+    w.timeout = 0.5;
+    w.start();
+
+    setTimeout(() => {
+      w.reset();
+    }, 250);
+
+    w.on('timeout', () => {
+      const passed = (new Date()) - start;
+
+      if (passed < 700) {
+        done.fail('Timer has not been restarted');
+      } else {
+        done();
+      }
+    });
+
+  });
+});
