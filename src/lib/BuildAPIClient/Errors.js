1e28ad91ee7c7abb09f9bc8a7a310697c4191312
Mikhail Yurasov <me@yurasov.me>
2016-02-15 04:41:24 -0800

    Formatting

--- src/lib/BuildAPIClient/Errors.js
+++ src/lib/BuildAPIClient/Errors.js
@@ -8,13 +8,13 @@ class BuildAPIError extends Error {
   constructor(message, id) {
     super(message || 'Build API error', id);
   }
-};
+}
 
 class TimeoutError extends BuildAPIError {
   constructor(message, id) {
     super(message || 'Build API timeout error', id);
   }
-};
+}
 
 module.exports.BuildAPIError = BuildAPIError;
 module.exports.TimeoutError = TimeoutError;
