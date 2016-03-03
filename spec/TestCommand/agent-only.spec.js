d6d5670a150287bbd80bccd0fe4b231313c19897
Mikhail Yurasov <me@yurasov.me>
2016-03-03 01:54:44 -0800

    Update agent-only test

--- spec/TestCommand/agent-only.spec.js
+++ spec/TestCommand/agent-only.spec.js
@@ -1,18 +1,25 @@
+/**
+ * Agent-only tests
+ */
+
 'use strict';
 
+require('jasmine-expect');
 const c = require('colors');
 const packageJson = require('../../package.json');
 const TestCommand = require('../../src/lib/Commands/TestCommand');
 const interceptStdout = require('intercept-stdout');
 
 describe('TestCommand test suite for agent-only scenario', () => {
-  let out = '';
+  let commandOut = '', commandSuccess = true;
 
   beforeEach(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
   });
 
+  // todo: extract command creation/run
   it('should run a command', (done) => {
+
     // create command
     const command = new TestCommand();
     command.version = packageJson.version;
@@ -23,17 +30,25 @@ describe('TestCommand test suite for agent-only scenario', () => {
     command.testCaseFile = __dirname + '/fixtures/agent-only/tests/agent.test.nut';
 
     console.log(c.inverse('======== command start ========'));
-    const finishMsg = () => console.log(c.inverse('========= command end ========='));
-    const unHook = interceptStdout(txt => { out += txt; });
 
-    command.run()
-      .then(unHook)
-      .then(finishMsg, finishMsg)
-      .then(done, done);
+    const unHook = interceptStdout(txt => {
+      commandOut += txt;
+    });
+
+    const onDone = (success) => {
+      unHook();
+      console.log(c.inverse('========= command end ========='));
+      commandSuccess = success;
+      done();
+    };
+
+    command.run().then(() => onDone(true), () => onDone(false));
   });
 
   it('should do something', (done) => {
-    // todo: insert checks here
+    // todo: insert more checks here
+    expect(commandOut).not.toBeEmptyString();
+    expect(commandSuccess).toBe(true);
     done();
   });
 });
