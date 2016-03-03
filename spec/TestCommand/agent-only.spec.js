a34c4c1fbca76b8c18adaaafc9ce217a899d4f13
Mikhail Yurasov <me@yurasov.me>
2016-03-03 02:14:08 -0800

    Extract command launch

--- spec/TestCommand/agent-only.spec.js
+++ spec/TestCommand/agent-only.spec.js
@@ -5,50 +5,31 @@
 'use strict';
 
 require('jasmine-expect');
-const c = require('colors');
-const packageJson = require('../../package.json');
-const TestCommand = require('../../src/lib/Commands/TestCommand');
-const interceptStdout = require('intercept-stdout');
+const run = require('./run');
 
 describe('TestCommand test suite for agent-only scenario', () => {
-  let commandOut = '', commandSuccess = true;
+  let commandOut = '',
+    commandSuccess = true;
 
   beforeEach(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
   });
 
-  // todo: extract command creation/run
   it('should run a command', (done) => {
-
-    // create command
-    const command = new TestCommand();
-    command.version = packageJson.version;
-    command.debug = false;
-    command.testFrameworkFile = __dirname + '/../../src/impUnit/index.nut';
-    // todo: update device/model from tests config or use single config
-    command.configPath = __dirname + '/fixtures/agent-only/.imptest';
-    command.testCaseFile = __dirname + '/fixtures/agent-only/tests/agent.test.nut';
-
-    console.log(c.inverse('======== command start ========'));
-
-    const unHook = interceptStdout(txt => {
-      commandOut += txt;
-    });
-
-    const onDone = (success) => {
-      unHook();
-      console.log(c.inverse('========= command end ========='));
-      commandSuccess = success;
+    run({
+      configPath:  '/fixtures/agent-only/.imptest',
+      testCaseFile:  '/fixtures/agent-only/tests/agent.test.nut'
+    }).then((res) => {
+      commandSuccess = res.success;
+      commandOut = res.out;
       done();
-    };
-
-    command.run().then(() => onDone(true), () => onDone(false));
+    });
   });
 
   it('should do something', (done) => {
     // todo: insert more checks here
-    expect(commandOut).not.toBeEmptyString();
     expect(commandSuccess).toBe(true);
+    expect(commandOut).not.toBeEmptyString();
     done();
   });
 });
---
+++ spec/TestCommand/run.js
@@ -0,0 +1,46 @@
+/**
+ * Runs test command with options
+ */
+
+'use strict';
+
+const c = require('colors');
+const packageJson = require('../../package.json');
+const TestCommand = require('../../src/lib/Commands/TestCommand');
+const interceptStdout = require('intercept-stdout');
+
+function run(options) {
+
+  return new Promise((resolve, reject) => {
+
+    // create command
+    const command = new TestCommand();
+    command.version = packageJson.version;
+    command.debug = false;
+    command.testFrameworkFile = __dirname + '/../../src/impUnit/index.nut';
+    // todo: update device/model from tests config or use single config
+    command.configPath = __dirname + options.configPath;
+    command.testCaseFile = __dirname + options.testCaseFile;
+
+    console.log(c.inverse('======== command start ========'));
+
+    let out = '';
+
+    // intercept stdout
+    const unHook = interceptStdout(txt => {
+      out += txt;
+    });
+
+    const onDone = (success) => {
+      unHook();
+      console.log(c.inverse('========= command end ========='));
+      resolve({success, out});
+    };
+
+    // run command
+    command.run().then(() => onDone(true), () => onDone(false));
+    
+  });
+}
+
+module.exports = run;
