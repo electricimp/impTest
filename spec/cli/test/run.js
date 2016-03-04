fd5c533fa1ee3ce01d420eff895f4989ad1fbb5a
Mikhail Yurasov <me@yurasov.me>
2016-03-03 22:24:25 -0800

    Use devices/model from testing config

--- spec/cli/test/run.js
+++ spec/cli/test/run.js
@@ -4,12 +4,33 @@
 
 'use strict';
 
+const fs = require('fs');
 const c = require('colors');
 const utils = require('../../utils');
+const config = require('../../support/config');
 const interceptStdout = require('intercept-stdout');
 const packageJson = require('../../../package.json');
 const TestCommand = require('../../../src/lib/Commands/TestCommand');
 
+/**
+ * Create temporary .imptest file with config settings applied
+ * @param {string} path
+ * @return {string}
+ */
+function createTmpImpTestFile(path) {
+  let s = fs.readFileSync(path, 'utf-8');
+  s = s.replace('<modelId>', config.cli.modelId);
+  s = s.replace('<deviceIds>', JSON.stringify(config.cli.deviceIds));
+  path += '.tmp';
+  fs.writeFileSync(path, s, 'utf-8');
+  return path;
+}
+
+/**
+ * Run command
+ * @param options
+ * @return {Promise}
+ */
 function run(options) {
 
   return new Promise((resolve, reject) => {
@@ -20,7 +41,7 @@ function run(options) {
     command.debug = false;
     command.testFrameworkFile = __dirname + '/../../../src/impUnit/index.nut';
     // todo: update device/model from tests config or use single config
-    command.configPath = __dirname + options.configPath;
+    command.configPath = createTmpImpTestFile(__dirname + options.configPath);
 
     // optional options
     options.testCaseFile !== undefined && (command.testCaseFile = __dirname + options.testCaseFile);
